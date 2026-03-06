cd09d510c957cc17d9e198b237f5afeb1468deb2
AprilGar <april.garlington@hotmail.co.uk>
2026-03-06 15:40:22 +0000

    begin making models

---
+++ app/uk/gov/hmrc/senioraccountingofficerstubs/models/Company.scala
@@ -0,0 +1,19 @@
+package uk.gov.hmrc.senioraccountingofficerstubs.models
+
+import play.api.libs.json.{Json, OFormat}
+
+import java.time.LocalDate
+
+case class Company (
+                     companyName: String,
+                     uniqueTaxReference: String,
+                     companyReferenceNumber: String,
+                     companyType: String,
+                     financialYearEndDate: LocalDate,
+                     seniorAccountingOfficers: List[SeniorAccountingOfficer]
+                   )
+
+object Company {
+  implicit val CompanyFormat: OFormat[Company] = Json.format[Company]
+}
+
--- app/uk/gov/hmrc/senioraccountingofficerstubs/models/NotificationRequest.scala
+++ app/uk/gov/hmrc/senioraccountingofficerstubs/models/NotificationRequest.scala
@@ -3,9 +3,11 @@ package uk.gov.hmrc.senioraccountingofficerstubs.models
 import play.api.libs.json.{Json, OFormat}
 
 case class NotificationRequest (
-
+    companies: List[Company],
+    additionalInformation: Option[String]
                                      )
 
 object NotificationRequest {
   implicit val NotificationRequestFormat: OFormat[NotificationRequest] = Json.format[NotificationRequest]
 }
+
