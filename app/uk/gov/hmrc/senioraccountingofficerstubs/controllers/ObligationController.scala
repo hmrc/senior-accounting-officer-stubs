2eb7670c6979daf4e5fe2e2c467277d0de6ea837
Jacob Leeming <235530878+jacob-leeming-netcompany@users.noreply.github.com>
2026-03-09 12:10:06 +0000

    obligation get endpoint (#6)
    * obligation get endpoint
    
    * rename obligation controller
    
    * add missing array
    
    * address comments
    
    * address comments
    
    * generate crn/utr
    
    * integration test the obligation controller
    
    * format routes file
    
    * make synchronous
    
    * move obligation auth filter tests
    
    * use appropriate test data
    
    * change unit testing json approach
    
    * lint
    
    * todo removed

---
+++ app/uk/gov/hmrc/senioraccountingofficerstubs/controllers/ObligationController.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.senioraccountingofficerstubs.controllers
+
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.domain.SaUtrGenerator
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import scala.util.Random
+
+import javax.inject.Inject
+
+class ObligationController @Inject() (cc: ControllerComponents) extends BackendController(cc) {
+
+  private val stubbedSaoSubscriptionId = "123"
+
+  private val stubbedObligationPayload = Json.obj(
+    "saoSubscriptionId" -> stubbedSaoSubscriptionId,
+    "subscription"      -> Json.obj(
+      "subscriptionTimestamp"     -> "2021-01-01T00:00:00Z",
+      "companyRegistrationNumber" -> generateCrn,
+      "uniqueTaxReference"        -> generateUtr,
+      "companyName"               -> "Testdata Company Ltd",
+      "contacts"                  -> Json.arr(
+        Json.obj("name" -> "Firstname Middlename Lastname", "email" -> "example@example.com")
+      )
+    ),
+    "submissions" -> Json.arr(
+      Json.obj(
+        "financialYearEnd" -> 2025,
+        "notification"     -> Json.obj("id" -> "notificationId", "notificationTimestamp" -> "2021-01-01T00:00:00Z"),
+        "certificate"      -> Json.obj("id" -> "certificateId", "certificateTimestamp" -> "2021-01-01T00:00:00Z")
+      )
+    )
+  )
+
+  def getObligation(saoSubscriptionId: String): Action[AnyContent] = Action { implicit request =>
+    if saoSubscriptionId == stubbedSaoSubscriptionId then {
+      Ok(stubbedObligationPayload)
+    } else {
+      NotFound
+    }
+  }
+
+  private def generateCrn = {
+    val num = Random.nextInt(1000000)
+    f"$num%010d"
+  }
+
+  private def generateUtr = {
+    val seed = Random.nextInt(1000000)
+    SaUtrGenerator(seed).nextSaUtr
+  }
+}
---
+++ test/uk/gov/hmrc/senioraccountingofficerstubs/controllers/ObligationControllerSpec.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.senioraccountingofficerstubs.controllers
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.http.Status
+import play.api.libs.json.JsArray
+import play.api.test.Helpers.*
+import play.api.test.{FakeRequest, Helpers}
+
+class ObligationControllerSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite {
+
+  private val fakeGETRequest = FakeRequest("GET", "/obligation")
+  private val controller     = app.injector.instanceOf[ObligationController]
+
+  private val knownId   = "123"
+  private val unknownId = "567"
+
+  "GET /obligation/:saoSubscriptionId" should {
+    "return 200 and obligation for a known saoSubscriptionId" in {
+      val result = controller.getObligation(knownId)(fakeGETRequest)
+
+      status(result) shouldBe Status.OK
+      val json = contentAsJson(result)
+
+      (json \ "saoSubscriptionId").as[String] shouldBe knownId
+
+      val subscription = json \ "subscription"
+      (subscription \ "subscriptionTimestamp").as[String] shouldBe "2021-01-01T00:00:00Z"
+      (subscription \ "companyRegistrationNumber").as[String] should fullyMatch regex """\d{10}"""
+      (subscription \ "uniqueTaxReference").as[String] should fullyMatch regex """\d{10}"""
+      (subscription \ "companyName").as[String] shouldBe "Testdata Company Ltd"
+
+      val contacts = (subscription \ "contacts").as[JsArray]
+      (contacts(0) \ "name").as[String] shouldBe "Firstname Middlename Lastname"
+      (contacts(0) \ "email").as[String] shouldBe "example@example.com"
+
+      val submissions     = (json \ "submissions").as[JsArray]
+      val firstSubmission = submissions(0)
+      (firstSubmission \ "financialYearEnd").as[Int] shouldBe 2025
+
+      val notification = firstSubmission \ "notification"
+      (notification \ "id").as[String] shouldBe "notificationId"
+      (notification \ "notificationTimestamp").as[String] shouldBe "2021-01-01T00:00:00Z"
+
+      val certificate = firstSubmission \ "certificate"
+      (certificate \ "id").as[String] shouldBe "certificateId"
+      (certificate \ "certificateTimestamp").as[String] shouldBe "2021-01-01T00:00:00Z"
+    }
+
+    "return a 404 for an unknown saoSubscriptionId" in {
+      val result = controller.getObligation(unknownId)(fakeGETRequest)
+      status(result) shouldBe Status.NOT_FOUND
+    }
+  }
+}
